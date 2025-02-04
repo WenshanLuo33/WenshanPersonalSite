85b089b65597759fac896a56064f8a134f9acf8b
WenshanLuo33 <wl763@cornell.edu>
2025-02-04 01:16:35 -0500

    Add files via upload

---
+++ Assignment_01/map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = "pk.eyJ1Ijoid2VuMyIsImEiOiJjbTZpZGk0dDYwNnhqMm1vaGNtc2pyc29pIn0.R1YUPm_wlHzWpnWvsp8dIQ";
+
+const map = new mapboxgl.Map({
+    container: "map", // ✅ 添加逗号
+    style: "mapbox://styles/wen3/cm6q2kgh700xi01qq9nlq2i76", // ✅ 添加逗号
+    zoom: 10,
+    center: [-74, 70.725]
+});
